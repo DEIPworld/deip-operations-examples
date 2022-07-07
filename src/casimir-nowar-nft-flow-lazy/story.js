06f0e3a61b78451a0d5dc31c8de9d462b8b8aca9
Egor <egor.anekey@deip.world>
2022-07-07 05:11:17 +0300

    feat: casimir setup script

--- src/casimir-nowar-nft-flow-lazy/story.js
+++ src/casimir-nowar-nft-flow-lazy/story.js
@@ -34,9 +34,9 @@ async function run() {
   const api = chainService.getChainNodeClient();
   const rpc = chainService.getChainRpc();
 
-  const DEIP_APPCHAIN_CORE_ASSET = config.DEIP_APPCHAIN_CORE_ASSET;
-  const DAO_SEED_FUNDING_AMOUNT = config.DAO_SEED_FUNDING_AMOUNT
-  const DAO_FUNDING_AMOUNT = config.DAO_FUNDING_AMOUNT;
+  const CORE_ASSET = config.CORE_ASSET;
+  const DAO_SEED_FUNDING_AMOUNT = config.FAUCET_ACCOUNT.fundingAmount
+  const DAO_FUNDING_AMOUNT = config.FAUCET_ACCOUNT.fundingAmount;
 
   /**
    * Create Alice DAO actor
@@ -110,7 +110,7 @@ async function run() {
   logJsonResult(`Bob DAO created`, bobDao);
 
 
-  const aliceDaoCoreAssetBalance1 = await rpc.getFungibleTokenBalanceByOwnerAsync(aliceDaoId, DEIP_APPCHAIN_CORE_ASSET.id);
+  const aliceDaoCoreAssetBalance1 = await rpc.getFungibleTokenBalanceByOwnerAsync(aliceDaoId, CORE_ASSET.id);
   logJsonResult(`Alice Dao CoreAsset balance after CreateProjectCmd`, aliceDaoCoreAssetBalance1);
 
 
@@ -331,9 +331,9 @@ async function run() {
       const transferFt1 = new TransferFungibleTokenCmd({
         from: buyerDaoId,
         to: moderatorDaoId,
-        tokenId: DEIP_APPCHAIN_CORE_ASSET.id,
-        symbol: DEIP_APPCHAIN_CORE_ASSET.symbol,
-        precision: DEIP_APPCHAIN_CORE_ASSET.precision,
+        tokenId: CORE_ASSET.id,
+        symbol: CORE_ASSET.symbol,
+        precision: CORE_ASSET.precision,
         amount: "99999"
       });
 
